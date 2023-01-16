3e53cadaab593a0b04508c8660aa11958943b46b
Nauka <nauka14@gmail.com>
2023-01-16 11:41:27 +0200

    Building router

---
+++ controllers/index.php
@@ -0,0 +1,5 @@
+<?php
+
+$tasks = $db->selectAll('tasks');
+
+require_once('views/index.view.php');
--- views/index.view.php
+++ views/index.view.php
@@ -7,6 +7,16 @@
     <title>Document</title>
 </head>
 <body>
+    
+    <nav>
+        <ul>
+            <li><a href="about">About</a></li>
+            <li><a href="about/culture">About culture</a></li>
+            <li><a href="contact">Contact</a></li>
+        </ul>
+    </nav>
+    <h1>My Tasks</h1>
+
     <ul>
         <?php foreach ( $tasks as $task ): ?>
             <li>
